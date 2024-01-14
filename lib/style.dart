0f6e13a2dde35cc0056f357449f5521a26aa3026
Yu-jimin <jiminyu0114@gmail.com>
2024-01-15 00:22:02 +0900

    chore: 프로젝트 초기 세팅

---
+++ lib/style.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TitleKorean = TextStyle(
+      fontFamily: 'Pretendard',
+      fontWeight: FontWeight.w700,
+      fontSize: 23,
+  );
+  static const TitleEnglish = TextStyle(
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w600,
+      fontSize: 23,
+      letterSpacing: -0.5,
+  );
+  static const SemiTitle = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+  );
+  static const bodyBold = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 15,
+  );
+  static const bodyRegular = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+  );
+}
+
+class ColorStyles {
+  static const Color mainBlue = Color(0x0083ee);
+  static const Color black = Color(0x000000);
+  static const Color grey = Color(0x767676);
+}
