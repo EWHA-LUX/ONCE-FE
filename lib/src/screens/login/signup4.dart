44d4b9db2ea1a961b983f8eed7bc256285384503
Yu-jimin <jiminyu0114@gmail.com>
2024-01-30 22:53:22 +0900

    feat: 검색창 구현 및 전체 레이아웃 수정 (#14)

--- lib/src/screens/login/signup4.dart
+++ lib/src/screens/login/signup4.dart
@@ -15,7 +15,35 @@ class _Signup4State extends State<Signup4> {
 
   List<bool> isCardSelectedList = List.generate(6, (index) => false);
   List<bool> isCardCompanyList = List.generate(2, (index) => false);
+  List<String> filteredCardNames = [];
 
+  TextEditingController _searchController = TextEditingController();
+
+  List<String> allCardNames = [
+    '현대카드 M Boost',
+    '현대카드 ZERO Edition2',
+    '현대카드M Edition3',
+    '우리동네 GS 삼성카드',
+    '국만행복 삼성체크카드 v2',
+    '롯데카드 M Boost',
+    '롯데카드 ZERO Edition2',
+    '롯데카드M Edition3',
+    '롯데카드 GS 삼성카드',
+    '롯데카드 삼성체크카드 v2',
+    // Add other card names
+  ];
+
+  // 카드 이름 검색창
+  void filterCardNames(String query) {
+    setState(() {
+      filteredCardNames = allCardNames
+          .where((cardName) =>
+          cardName.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  // 카드 회사명 상단 탭
   Widget _cardCompany(int index, String iconPath, String cardCompany) {
     return InkWell(
       onTap: () {
@@ -70,7 +98,7 @@ class _Signup4State extends State<Signup4> {
     );
   }
 
-
+  // 하단 카드 컨테이너
   Widget _cardContainer(int index, String iconPath, String cardType, String cardName) {
     return InkWell(
       onTap: () {
@@ -99,16 +127,19 @@ class _Signup4State extends State<Signup4> {
           ),
           Row(
             children: [
-              SvgPicture.asset(
-                iconPath,
-                width: 58,
-                height: 91,
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 15),
+                child: SvgPicture.asset(
+                  iconPath,
+                  width: 80,
+                  height: 48,
+                ),
               ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(left: 70, top: 0),
+                    padding: const EdgeInsets.only(left: 40, top: 15),
                     child: Text(
                       cardType,
                       style: const TextStyle(
@@ -120,7 +151,7 @@ class _Signup4State extends State<Signup4> {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(left: 70, top: 5),
+                    padding: const EdgeInsets.only(left: 40, top: 5),
                     child: Text(
                       cardName,
                       style: const TextStyle(
@@ -149,7 +180,7 @@ class _Signup4State extends State<Signup4> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Positioned(
+            Container(
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Column(
@@ -195,17 +226,50 @@ class _Signup4State extends State<Signup4> {
                       style: TextStyles.SemiTitle,
                     ),
                     const SizedBox(
-                      height: 30,
+                      height: 25,
                     ),
-                    // 검색창 만들기
+                    Container(
+                      width: 337,
+                      height: 38,
+                      child: TextField(
+                        controller: _searchController,
+                        onChanged: filterCardNames,
+                        decoration: InputDecoration(
+                          hintText: '연결하고 싶은 카드를 검색해보세요.',
+                          hintStyle: const TextStyle(
+                            fontFamily: 'Pretendard',
+                            fontSize: 13,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.grey,
+                          ),
+                          suffixIcon: const Icon(
+                            Icons.search,
+                            color: Colors.blue,
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18),
+                            borderSide: BorderSide.none,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
             ),
+            const SizedBox(
+              height: 20,
+            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Positioned(
+                Container(
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 25.0),
                     child: Column(
@@ -219,15 +283,19 @@ class _Signup4State extends State<Signup4> {
                           ],
                         ),
                         const SizedBox(
-                          height: 20,
+                          height: 25,
                         ),
                         Column(
                           children: [
-                            _cardContainer(0, "assets/iages/card_logo/hyundai_logo.svg", "신용카드", "현대카드 M Boost"),
-                            _cardContainer(1, "assets/iages/card_logo/hyundai_logo.svg", "신용카드", "현대카드 ZERO Edition2"),
-                            _cardContainer(2, "assets/iages/card_logo/hyundai_logo.svg", "신용카드", "현대카드M Edition3"),
-                            _cardContainer(3, "assets/iages/card_logo/hyundai_logo.svg", "체크카드", "우리동네 GS 삼성카드"),
-                            _cardContainer(4, "assets/iages/card_logo/hyundai_logo.svg", "체크카드", "국만행복 삼성체크카드 v2"),
+                            _cardContainer(0, "assets/images/card_logo/card_example_img.svg", "신용카드", allCardNames[0]),
+                            const SizedBox(height: 10),
+                            _cardContainer(1, "assets/images/card_logo/card_example_img.svg", "신용카드", allCardNames[1]),
+                            const SizedBox(height: 10),
+                            _cardContainer(2, "assets/images/card_logo/card_example_img.svg", "신용카드", allCardNames[2]),
+                            const SizedBox(height: 10),
+                            _cardContainer(3, "assets/images/card_logo/card_example_img.svg", "체크카드", allCardNames[3]),
+                            const SizedBox(height: 10),
+                            _cardContainer(4, "assets/images/card_logo/card_example_img.svg", "체크카드", allCardNames[4]),
                           ],
                         ),
                       ],
@@ -237,7 +305,7 @@ class _Signup4State extends State<Signup4> {
               ],
             ),
             const SizedBox(
-              height: 35,
+              height: 50,
             ),
             Positioned(
               child: Padding(
