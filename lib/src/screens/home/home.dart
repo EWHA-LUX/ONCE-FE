d422d5251a116c32b4513cd5202cf025860e3b65
Lee Haewon <hawon2205@ewhain.net>
2024-04-16 13:44:41 +0900

    feat: 카드 추천 API 연결 (#64)

--- lib/src/screens/home/home.dart
+++ lib/src/screens/home/home.dart
@@ -20,11 +20,20 @@ class _HomeState extends State<Home> {
   // ==================== API 통신 ====================
   final String BASE_URL = Constants.baseUrl;
 
+  bool isRecommend = false;
+
   String nickname = '';
   int ownedCardCount = 0;
   List<dynamic> keywordList = [];
 
-  late Future<void> _homeInfoFuture;
+  int chatId = 0;
+  String cardName = '';
+  String cardImg = '';
+  String benefit = '';
+  int discount = 0;
+
+  late Future<void> _homeInfoFuture; // 홈 기본 정보
+  late Future<void> _cardRecommendFuture; // 카드 추천
 
   @override
   void initState() {
@@ -33,11 +42,21 @@ class _HomeState extends State<Home> {
     _homeInfoFuture = _homeInfo(context);
   }
 
-  void _updateState(Map<dynamic, dynamic> responseData) {
+  void _updateHomeInfoState(Map<dynamic, dynamic> responseData) {
     setState(() {
       nickname = responseData['result']['nickname'];
       ownedCardCount = responseData['result']['ownedCardCount'];
       keywordList = responseData['result']['keywordList'];
+    });
+  }
+
+  void _updateCardRecommendState(Map<dynamic, dynamic> responseData) {
+    setState(() {
+      isRecommend = true;
+      cardName = responseData['result']['cardName'];
+      cardImg = responseData['result']['cardImg'];
+      benefit = responseData['result']['benefit'];
+      discount = responseData['result']['discount'];
     });
   }
 
@@ -59,7 +78,55 @@ class _HomeState extends State<Home> {
     print(responseData);
 
     if (responseData['code'] == 1000) {
-      _updateState(responseData);
+      _updateHomeInfoState(responseData);
+    }
+  }
+
+  // [Get] 챗봇 카드 추천
+  Future<void> _cardRecommend(BuildContext context, String keyword,
+      int paymentAmount) async {
+    final String apiUrl = '${BASE_URL}/home';
+
+    const storage = FlutterSecureStorage();
+    String? storedAccessToken = await storage.read(key: 'accessToken');
+
+    final baseOptions = BaseOptions(
+      headers: {'Authorization': 'Bearer $storedAccessToken'},
+    );
+
+    final dio = Dio(baseOptions);
+
+    try {
+      var response = await dio.get(apiUrl, queryParameters: {
+        'keyword': keyword,
+        'paymentAmount': paymentAmount
+      });
+      Map<dynamic, dynamic> responseData = response.data;
+      print(responseData);
+
+      if (responseData['code'] == 1000) {
+        _updateCardRecommendState(responseData);
+      }
+    } catch (e) {
+      // ** 차후 수정 필요 **
+      print(e.toString());
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("오류 발생"),
+            content: Text("서버와 통신 중 오류가 발생했습니다."),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("확인"),
+              ),
+            ],
+          );
+        },
+      );
     }
   }
 
@@ -73,6 +140,7 @@ class _HomeState extends State<Home> {
 
   void resetState() {
     setState(() {
+      isRecommend = false;
       keyword = '';
       initFormattedTime = '';
       userInputFormattedTime = '';
@@ -261,7 +329,7 @@ class _HomeState extends State<Home> {
         style: ElevatedButton.styleFrom(
             elevation: 0,
             padding: const EdgeInsets.all(8),
-            minimumSize: const Size(10, 20),
+            minimumSize: Size(10, 20),
             backgroundColor: Colors.white,
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20.0))),
@@ -291,7 +359,7 @@ class _HomeState extends State<Home> {
               borderRadius: BorderRadius.circular(10.0))),
       onPressed: () {
         // 카드 추천 API - keyword, price
-        // _cardRecommend(context, keyword, paymentAmount);
+        _cardRecommend(context, keyword, paymentAmount);
       },
       child: Text(finishText,
           style: const TextStyle(
@@ -425,15 +493,83 @@ class _HomeState extends State<Home> {
               borderRadius: BorderRadius.circular(13)),
           child: Center(
               child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(keywordText),
-          )),
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(keywordText),
+              )),
         ),
         onTap: () {
           setState(() {
             keyword = keywordText;
           });
         },
+      ),
+    );
+  }
+
+  Widget _cardRecommendBox() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, left: 35.0, right: 35.0),
+      child: Column(
+        children: [
+          Container(
+            height: 40,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.topLeft,
+                colors: [
+                  Color(0xff4472fc),
+                  Color(0xff8877d5),
+                ],
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(13),
+                topRight: Radius.circular(13),
+              ),
+            ),
+            child: Center(
+              child: Text.rich(
+                  TextSpan(children: <TextSpan>[
+                    TextSpan(
+                      text: '$ownedCardCount개의 카드 중',
+                      style: const TextStyle(
+                          fontFamily: 'Pretendard',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white),
+                    ),
+                    const TextSpan(
+                      text: ' 가장 좋은 카드 하나',
+                      style: TextStyle(
+                          fontFamily: 'Pretendard',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                    const TextSpan(
+                      text: '를 알려드려요.',
+                      style: TextStyle(
+                          fontFamily: 'Pretendard',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white),
+                    ),
+                  ])
+
+              ),
+            ),
+          ),
+          Container(
+            height: 150,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(13),
+                bottomRight: Radius.circular(13),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -464,7 +600,8 @@ class _HomeState extends State<Home> {
         ),
         keyword.isNotEmpty
             ? _chatbotSecondSay('$keyword에서 결제하시는 군요!', userInputFormattedTime)
-            : const SizedBox()
+            : const SizedBox(),
+        isRecommend ? _cardRecommendBox() : const SizedBox()
       ],
     );
   }
@@ -633,38 +770,39 @@ class _HomeState extends State<Home> {
               ),
               Expanded(
                   child: TextField(
-                controller: userInputController,
-                decoration: InputDecoration(
-                    suffixIcon: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: GestureDetector(
-                        child: SvgPicture.asset(
-                          "assets/images/icons/send_icon.svg",
-                          width: 10,
+                    controller: userInputController,
+                    decoration: InputDecoration(
+                        suffixIcon: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: GestureDetector(
+                            child: SvgPicture.asset(
+                              "assets/images/icons/send_icon.svg",
+                              width: 10,
+                            ),
+                            onTap: () {
+                              userInputTime();
+                              setState(() {
+                                keyword = userInputController.text;
+                                userInputController.clear();
+                              });
+                            },
+                          ),
                         ),
-                        onTap: () {
-                          userInputTime();
-                          setState(() {
-                            keyword = userInputController.text;
-                            userInputController.clear();
-                          });
-                        },
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: const BorderSide(color: Color(0xffADADAD))),
-                    contentPadding: const EdgeInsets.symmetric(
-                      horizontal: 30,
-                      vertical: 10,
-                    ),
-                    hintText: '궁금한 것을 질문해 보세요.',
-                    hintStyle: const TextStyle(
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                        color: Color(0xffADADAD))),
-              )),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                                color: Color(0xffADADAD))),
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 10,
+                        ),
+                        hintText: '궁금한 것을 질문해 보세요.',
+                        hintStyle: const TextStyle(
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            color: Color(0xffADADAD))),
+                  )),
             ],
           ),
         ),
