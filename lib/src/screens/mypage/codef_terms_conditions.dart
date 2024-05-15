c1395cd43c9196889eb2f3f95c42b9521ce48006
Yu-jimin <jiminyu0114@gmail.com>
2024-05-15 19:33:21 +0900

    feat: CODEF 약관 동의 여부에 따라 활성화 상태 변경 (#53)

--- lib/src/screens/mypage/codef_terms_conditions.dart
+++ lib/src/screens/mypage/codef_terms_conditions.dart
@@ -11,6 +11,16 @@ class RegisterMainCard extends StatefulWidget {
 
 class _RegisterMainCardState extends State<RegisterMainCard> {
   bool isChecked = false;
+  bool isSecondChecked = false;
+  bool isThirdChecked = false;
+
+  bool get allChecked => isChecked && isSecondChecked && isThirdChecked;
+
+  void updateAllChecked() {
+    setState(() {
+      isChecked = isSecondChecked && isThirdChecked;
+    });
+  }
 
   @override
   Widget _termsArea(BuildContext context) {
@@ -90,11 +100,20 @@ class _RegisterMainCardState extends State<RegisterMainCard> {
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(left: 26),
-                    child: SvgPicture.asset(
-                      'assets/images/icons/terms_check_icon.svg',
-                      width: 25,
-                      height: 25,
-                      color: isChecked ? Colors.blue : null,
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isChecked = !isChecked;
+                          isSecondChecked = isChecked;
+                          isThirdChecked = isChecked;
+                        });
+                      },
+                      child: SvgPicture.asset(
+                        'assets/images/icons/terms_check_icon.svg',
+                        width: 25,
+                        height: 25,
+                        color: isChecked ? const Color(0xFF0083EE) : null,
+                      ),
                     ),
                   ),
                   const SizedBox(width: 16),
@@ -103,20 +122,27 @@ class _RegisterMainCardState extends State<RegisterMainCard> {
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          '전체 동의하기',
-                          style: TextStyle(
-                            fontFamily: 'Pretendard',
-                            fontSize: 13,
-                            fontWeight: FontWeight.w700,
-                            color: isChecked ? Colors.blue : null,
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isChecked = !isChecked;
+                            });
+                          },
+                          child: Text(
+                            '전체 동의하기',
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              fontSize: 13,
+                              fontWeight: FontWeight.w700,
+                              color: isChecked ? const Color(0xFF0083EE) : null,
+                            ),
                           ),
                         ),
                         const SizedBox(height: 5),
                         Text(
                           '전체동의는 선택목적에 대한 동의를 포함하고\n'
-                              '있으며, 선택목적에 대한 동의를 거부해도 서비스\n'
-                              '이용이 가능합니다.',
+                          '있으며, 선택목적에 대한 동의를 거부해도 서비스\n'
+                          '이용이 가능합니다.',
                           style: TextStyle(
                             fontFamily: 'Pretendard',
                             fontSize: 11,
@@ -144,47 +170,68 @@ class _RegisterMainCardState extends State<RegisterMainCard> {
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(left: 26),
-                    child: SvgPicture.asset(
-                      'assets/images/icons/terms_check_icon_single.svg',
-                      width: 22,
-                      height: 22,
-                      color: isChecked ? Colors.blue : Color(0xFFADADAD),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isSecondChecked = !isSecondChecked;
+                        });
+                      },
+                      child: SvgPicture.asset(
+                        'assets/images/icons/terms_check_icon_single.svg',
+                        width: 22,
+                        height: 22,
+                        color: isSecondChecked
+                            ? const Color(0xFF0083EE)
+                            : const Color(0xFFADADAD),
+                      ),
                     ),
                   ),
                   const SizedBox(width: 13),
-                  Container(
-                    width: 200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '동의합니다.',
-                          style: TextStyle(
-                            fontFamily: 'Pretendard',
-                            fontSize: 13,
-                            fontWeight: FontWeight.w700,
-                            color: isChecked ? Colors.blue : Color(0xFFADADAD),
+                  Expanded(
+                    child: Container(
+                      width: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                isSecondChecked = !isSecondChecked;
+                                updateAllChecked();
+                              });
+                            },
+                            child: Text(
+                              '동의합니다.',
+                              style: TextStyle(
+                                fontFamily: 'Pretendard',
+                                fontSize: 13,
+                                fontWeight: FontWeight.w700,
+                                color: isSecondChecked
+                                    ? const Color(0xFF0083EE)
+                                    : const Color(0xFFADADAD),
+                              ),
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          '원스 서비스는 민감한 개인정보를 수집하고 안전하'
-                              '게 보호하기 위해 최선의 노력을 기울이고 있습니'
-                              '다. 카드사 연결을 위해서는 다음의 내용에 동의해'
-                              '야 합니다.',
-                          style: TextStyle(
-                            fontFamily: 'Pretendard',
-                            fontSize: 10,
-                            fontWeight: FontWeight.w300,
-                            color: const Color(0xFFADADAD),
+                          const SizedBox(height: 4),
+                          Text(
+                            '원스 서비스는 민감한 개인정보를 수집하고 안전하'
+                                '게 보호하기 위해 최선의 노력을 기울이고 있습니'
+                                '다. 카드사 연결을 위해서는 다음의 내용에 동의해'
+                                '야 합니다.',
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              fontSize: 10,
+                              fontWeight: FontWeight.w300,
+                              color: const Color(0xFFADADAD),
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 20, left: 8), // 오른쪽 여백 추가
+                    padding: const EdgeInsets.only(top: 20, right: 15, left: 5),
                     child: SvgPicture.asset(
                       'assets/images/3d_icons/security_3d_icon.svg',
                       width: 51,
@@ -193,7 +240,6 @@ class _RegisterMainCardState extends State<RegisterMainCard> {
                   ),
                 ],
               ),
-
               const SizedBox(height: 16),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -209,11 +255,21 @@ class _RegisterMainCardState extends State<RegisterMainCard> {
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(left: 26),
-                    child: SvgPicture.asset(
-                      'assets/images/icons/terms_check_icon_single.svg',
-                      width: 22,
-                      height: 22,
-                      color: isChecked ? Colors.blue : null,
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isThirdChecked = !isThirdChecked;
+                          updateAllChecked();
+                        });
+                      },
+                      child: SvgPicture.asset(
+                        'assets/images/icons/terms_check_icon_single.svg',
+                        width: 22,
+                        height: 22,
+                        color: isThirdChecked
+                            ? const Color(0xFF0083EE)
+                            : const Color(0xFFADADAD),
+                      ),
                     ),
                   ),
                   const SizedBox(width: 13),
@@ -222,21 +278,30 @@ class _RegisterMainCardState extends State<RegisterMainCard> {
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          '동의합니다.',
-                          style: TextStyle(
-                            fontFamily: 'Pretendard',
-                            fontSize: 13,
-                            fontWeight: FontWeight.w700,
-                            color: isChecked ? Colors.blue : Color(0xFFADADAD),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isThirdChecked = !isThirdChecked;
+                            });
+                          },
+                          child: Text(
+                            '동의합니다.',
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              fontSize: 13,
+                              fontWeight: FontWeight.w700,
+                              color: isThirdChecked
+                                  ? const Color(0xFF0083EE)
+                                  : const Color(0xFFADADAD),
+                            ),
                           ),
                         ),
                         const SizedBox(height: 4),
                         Text(
                           '[필수] 원스 서비스 이용약관\n'
-                              '[필수] 개인정보 수집 및 이용 동의\n'
-                              '[필수] 개인정보 제3자 제공 동의\n'
-                              '[필수] 개인정보 제3자 제공 동의 (카드사)',
+                          '[필수] 개인정보 수집 및 이용 동의\n'
+                          '[필수] 개인정보 제3자 제공 동의\n'
+                          '[필수] 개인정보 제3자 제공 동의 (카드사)',
                           style: TextStyle(
                             fontFamily: 'Pretendard',
                             fontSize: 10,
@@ -251,35 +316,37 @@ class _RegisterMainCardState extends State<RegisterMainCard> {
               ),
               const SizedBox(height: 32),
               ElevatedButton(
-                onPressed: isChecked ? () {
-                  Navigator.of(context).pushNamed("/codef/2");
-                } : null, // 버튼 비활성화
+                onPressed: allChecked
+                    ? () {
+                        Navigator.of(context).pushNamed("/codef/2");
+                      }
+                    : null, // 모든 동의 항목이 체크되어야 버튼이 활성화
                 child: Text(
                   '동의하고 계속하기',
                   style: TextStyle(
                     fontFamily: 'Pretendard',
                     fontSize: 15,
                     fontWeight: FontWeight.w700,
-                    color: Colors.black,
+                    color: allChecked ? Colors.white : Colors.black,
                   ),
                 ),
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                        (Set<MaterialState> states) {
+                    (Set<MaterialState> states) {
                       if (states.contains(MaterialState.disabled)) {
-                        return Color(0xFFE5E5E5); // 비활성화
+                        return const Color(0xFFE5E5E5); // 비활성화
                       }
-                      return Color(0xFF0083EE); // 활성화
+                      return const Color(0xFF0083EE); // 활성화
                     },
                   ),
                   shape: MaterialStateProperty.resolveWith<OutlinedBorder>(
-                        (Set<MaterialState> states) {
-                          return RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(10),
-                              bottomRight: Radius.circular(10),
-                            ),
-                          );
+                    (Set<MaterialState> states) {
+                      return RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                      );
                     },
                   ),
                   minimumSize: MaterialStateProperty.all(Size.fromHeight(53)),
